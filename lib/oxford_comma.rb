13ce0e36c657bb8a2ee1ca1847ea55378b6d9d7b
Terrance Rose Jr <trose618@gmail.com>
2018-10-20 22:15:04 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,9 +1,3 @@
 def oxford_comma(array)
-  if array.length > 1
-    array[-2] = array[-2] << " and"
-    array.join(", ")
-    elsif array.length == 2
-      array.join(" and ")
-    else array
-  end
+  return array.join if array.length == 1
 end
